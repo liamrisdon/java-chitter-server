e1c7d0dcce2069a342ab3623324ba131a33690bd
Liam Risdon <liamrisdon22@gmail.com>
2023-11-24 12:25:43 +0000

    existing username test passed

--- chitter/src/main/java/com/chitter/server/controller/SignUpController.java
+++ chitter/src/main/java/com/chitter/server/controller/SignUpController.java
@@ -24,6 +24,9 @@ public class SignUpController {
             if (userRepository.existingEmailCheck(newUser.getEmail())){
                 return new ResponseEntity<>("Email already linked to an account", HttpStatus.BAD_REQUEST);
             }
+            if (userRepository.existingUsernameCheck(newUser.getUsername())){
+                return new ResponseEntity<>("Username already linked to an account", HttpStatus.BAD_REQUEST);
+            }
             userRepository.save(new User(newUser.getUsername(), newUser.getName(), newUser.getEmail(), newUser.getPassword()));
             return new ResponseEntity<>("User successfully signed up", HttpStatus.CREATED);
         } catch (Exception e) {
