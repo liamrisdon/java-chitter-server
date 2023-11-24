c0d49614fd6d1eb19cabc6bd8a87ad99e354c36f
Liam Risdon <liamrisdon22@gmail.com>
2023-11-24 11:33:08 +0000

    sign up functionality added

--- chitter/src/main/java/com/chitter/server/controller/SignUpController.java
+++ chitter/src/main/java/com/chitter/server/controller/SignUpController.java
@@ -1,4 +1,30 @@
 package com.chitter.server.controller;
 
+import com.chitter.server.model.User;
+import com.chitter.server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/signup")
 public class SignUpController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<User> signUpUser(@RequestBody User newUser){
+
+        try {
+            userRepository.save(new User(newUser.getUsername(), newUser.getName(), newUser.getEmail(), newUser.getPassword()));
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
